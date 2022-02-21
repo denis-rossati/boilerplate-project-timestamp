761c7f70f2dfa792094d6338060bee5ba619b228
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-02-21 20:54:22 -0300

    Organize the server module

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express');
+
 const app = express();
 const cors = require('cors');
 
-app.use(cors({optionsSuccessStatus: 200}));
+app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
-app.get('/api/hello', function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api/hello', (req, res) => {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/:timestamp', (req, res) => {
+  const { timestamp } = req.params;
+});
+
+app.get('/api', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log(`Api listening port ${PORT}`);
 });
+
+module.exports = app;
