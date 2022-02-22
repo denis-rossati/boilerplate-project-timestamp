a2bacb2aa44fccf0608e0da4477bdaca37643ddf
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-02-21 21:17:37 -0300

    Add implementation for false cases in function isUnixTime

--- timestamp.js
+++ timestamp.js
@@ -1,9 +1,17 @@
-const isInUnixTime = (timestamp) => {
-
+// for this function I didn't want to make a regex or several if's
+// this question helped a lot:
+// https://stackoverflow.com/questions/1353684/detecting-an-invalid-date-date-instance-in-javascript
+const dateIsValid = (date) => {
+  return date instanceof Date && !isNaN(date);
 };
 
-const dateIsValid = (date) => {
-
+const isUnixTime = (timestamp) => {
+  try {
+    const date = new Date(timestamp * 1000).toISOString().slice(0, 10);
+    return dateIsValid(date);
+  } catch {
+    return false;
+  }
 };
 
 const convertTimestamp = (timestamp) => {
@@ -11,7 +19,7 @@ const convertTimestamp = (timestamp) => {
 };
 
 module.exports = {
-  isInUnixTime,
+  isUnixTime,
   convertTimestamp,
   dateIsValid,
 };
